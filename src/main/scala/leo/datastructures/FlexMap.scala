9e1d32bc09bcc00ffbf3da6b9e71e617aae58f6b
Alexander Steen <a.steen@fu-berlin.de>
2021-01-11 23:59:13 +0100

    more work on modal embedding

--- src/main/scala/leo/datastructures/FlexMap.scala
+++ src/main/scala/leo/datastructures/FlexMap.scala
@@ -10,6 +10,7 @@ trait FlexMap {
   def apply[A,B](key: Key[A,B]): mutable.Map[A, B]
 
   def setDefault[A,B](key: Key[A,B], default: B): Unit
+  def getDefault[A,B](key: Key[A,B]): Option[B]
 }
 object FlexMap {
   def apply(): FlexMap = new FlexMap {
@@ -17,6 +18,7 @@ object FlexMap {
     override type Key[+A,-B] = Int
 
     private[this] var currentKey: Key[Any, Any] = 0
+    private[this] final val defaults0: mutable.Map[Key[Any,Any], Any] = mutable.Map.empty
     private[this] final val map0: mutable.Map[Key[Any,Any], mutable.Map[Any,Any]] = mutable.Map.empty
 
     override final def createKey[A, B](): Key[A,B] = {
@@ -29,6 +31,7 @@ object FlexMap {
     override final def createKeyWithDefault[A, B](default: B): Key[A,B] = {
       val result = currentKey
       map0 += (result -> mutable.Map[Any,Any]().withDefaultValue(default))
+      defaults0 += (result -> default)
       currentKey += 1
       result
     }
@@ -36,7 +39,10 @@ object FlexMap {
     override final def setDefault[A,B](key: Key[A,B], default: B): Unit = {
       val map: mutable.Map[Any,Any] = apply(key)
       map0 += (key -> map.withDefaultValue(default))
+      defaults0 += (key -> default)
     }
+
+    def getDefault[A,B](key: Key[A,B]): Option[B] = defaults0.get(key).asInstanceOf[Option[B]]
 
     override final def apply[A, B](key: Key[A,B]): mutable.Map[A, B] = {
       map0(key).asInstanceOf[mutable.Map[A,B]]
--- src/main/scala/leo/modules/embeddings/ModalEmbedding.scala
+++ src/main/scala/leo/modules/embeddings/ModalEmbedding.scala
@@ -23,14 +23,17 @@ object ModalEmbedding {
   /////////////////////////////////////////////////////////////////////////////////
   private[this] final class ModalEmbeddingImpl extends Embedding[EmbeddingOption] {
 
+    ///////////////////////////////////////////////////////////////////
     private final val state = FlexMap.apply()
 
+    // Embedding options
     private final val POLYMORPHISM = state.createKeyWithDefault[Nothing, Boolean](default = false)
 
     private final val EMBEDDING_SYNTACTICAL = false
     private final val EMBEDDING_SEMANTICAL = true
     private final val EMBEDDINGTYPE = state.createKeyWithDefault[Nothing, Boolean](default = EMBEDDING_SEMANTICAL)
 
+    // Semantics dimensions
     private final val RIGIDITY_RIGID = true
     private final val RIGIDITY_FLEXIBLE = false
     private final val RIGIDITY = state.createKey[String, Boolean]()
@@ -47,6 +50,7 @@ object ModalEmbedding {
     private final val DOMAIN = state.createKey[String, Int]()
 
     private final val MODALS = state.createKey[String, Seq[String]]()
+    ////////////////////////////////////////////////////////////////////
 
     def apply(problem: Seq[AnnotatedFormula], embeddingOptions: Set[EmbeddingOption]): Seq[AnnotatedFormula] = {
       val (spec, remainingFormulas) = splitInput(problem)
@@ -54,7 +58,7 @@ object ModalEmbedding {
       val (typeFormulas, nonTypeFormulas) = remainingFormulas.partition(_.role == "type")
       val convertedTypeFormulas = typeFormulas.map(convertTypeFormula)
       val convertedOtherFormulas = nonTypeFormulas.map(convertAnnotatedFormula)
-      val generatedMetaFormulas: Seq[AnnotatedFormula] = Seq.empty // generate from state
+      val generatedMetaFormulas: Seq[AnnotatedFormula] = generateMetaFormulas()
 
       generatedMetaFormulas ++ convertedTypeFormulas ++ convertedOtherFormulas
     }
@@ -107,7 +111,11 @@ object ModalEmbedding {
 
         case THF.BinaryFormula(App, left, right) =>
           left match {
-            case THF.FunctionTerm("$box_int", Seq()) => mbox_int // TODO
+            case THF.FunctionTerm("$box_int", Seq()) =>
+              right match {
+                case THF.NumberTerm(TPTP.Integer(value)) => multiModal(value.toString)
+                  THF.BinaryFormula(App, mbox, THF.FunctionTerm(value.toString, Seq.empty)) // TODO
+              }
             case THF.FunctionTerm("$dia_int", Seq()) => ???
             case THF.FunctionTerm("$box_i", Seq()) => ???
             case THF.FunctionTerm("$dia_i", Seq()) => ???
@@ -178,18 +186,9 @@ object ModalEmbedding {
         case THF.! =>
           try {
             state(DOMAIN)(typ.pretty) match {
-              case DOMAIN_CONSTANT =>
-                if (state(POLYMORPHISM)()) {
-                  "mforall_const"
-                } else {
-                  s"mforall_const_${serializeType(convertedType)}"
-                }
+              case DOMAIN_CONSTANT => if (state(POLYMORPHISM)()) "mforall_const" else s"mforall_const_${serializeType(convertedType)}"
               case _ => // all three other cases
-                if (state(POLYMORPHISM)()) {
-                  "mforall_vary"
-                } else {
-                  s"mforall_vary_${serializeType(convertedType)}"
-                }
+                if (state(POLYMORPHISM)()) "mforall_vary" else s"mforall_vary_${serializeType(convertedType)}"
             }
           } catch {
             case _: NoSuchElementException => throw new EmbeddingException(s"Undefined domain semantics for type '${typ.pretty}'. Maybe a default value was omitted?")
@@ -198,18 +197,9 @@ object ModalEmbedding {
         case THF.? =>
           try {
             state(DOMAIN)(typ.pretty) match {
-              case DOMAIN_CONSTANT =>
-                if (state(POLYMORPHISM)()) {
-                  "mexists_const"
-                } else {
-                  s"mexists_const_${serializeType(convertedType)}"
-                }
+              case DOMAIN_CONSTANT => if (state(POLYMORPHISM)()) "mexists_const" else s"mexists_const_${serializeType(convertedType)}"
               case _ => // all three other cases
-                if (state(POLYMORPHISM)()) {
-                  "mexists_vary"
-                } else {
-                  s"mexists_vary_${serializeType(convertedType)}"
-                }
+                if (state(POLYMORPHISM)()) "mexists_vary" else s"mexists_vary_${serializeType(convertedType)}"
             }
           } catch {
             case _: NoSuchElementException => throw new EmbeddingException(s"Undefined domain semantics for type '${typ.pretty}'. Maybe a default value was omitted?")
@@ -267,6 +257,115 @@ object ModalEmbedding {
       }
     }
 
+    private def generateMetaFormulas(): Seq[TPTP.AnnotatedFormula] = {
+      import scala.collection.mutable
+      import modules.input.TPTPParser.annotatedTHF
+
+      val result: mutable.Buffer[TPTP.AnnotatedFormula] = mutable.Buffer.empty
+
+      // First: Introduce world type
+      result.append(worldTypeTPTPDef())
+
+      // Then: Introduce mrel (as relation or as collection of relations)
+      if (isMultiModal) result.append(indexedAccessibilityRelationTPTPDef("sometype")) // TODO
+      else result.append(simpleAccessibilityRelationTPTPDef())
+
+      // Then: Define mglobal/mlocal
+      state.getDefault(CONSEQUENCE) match {
+        case Some(consequence) => consequence match { // Add default and the other one if used
+          case CONSEQUENCE_GLOBAL =>
+            result.appendAll(mglobalTPTPDef())
+            if (state(CONSEQUENCE).exists(_._2 == CONSEQUENCE_LOCAL)) result.appendAll(mlocalTPTPDef())
+          case CONSEQUENCE_LOCAL =>
+            result.appendAll(mlocalTPTPDef())
+            if (state(CONSEQUENCE).exists(_._2 == CONSEQUENCE_LOCAL)) result.appendAll(mglobalTPTPDef())
+        }
+        case None => // Add only those used
+          if (state(CONSEQUENCE).exists(_._2 == CONSEQUENCE_LOCAL)) result.appendAll(mlocalTPTPDef())
+          if (state(CONSEQUENCE).exists(_._2 == CONSEQUENCE_LOCAL)) result.appendAll(mglobalTPTPDef())
+      }
+
+      // Then: Define connectives and modal operators
+      result.appendAll(connectivesTPTPDef())
+      if (isMultiModal) result.appendAll(indexedModalOperatorsTPTPDef("sometype")) // TODO
+      else result.appendAll(simpleModalOperatorsTPTPDef())
+
+      // Then: Give mrel properties (sem/syn)
+      // write used properties and assign (if semantical)
+      // or write syntactical axioms (if syntactical)
+
+      // Then: Define quantifiers (TH0/TH1)
+
+      // Then: Define quantifier restrictions (if cumul/decr)
+
+      // Return all
+      result.toSeq
+    }
+
+    private[this] def worldTypeTPTPDef(): TPTP.AnnotatedFormula = {
+      import modules.input.TPTPParser.annotatedTHF
+      annotatedTHF("thf(mworld_type, type, mworld: $tType).")
+    }
+
+    private[this] def simpleAccessibilityRelationTPTPDef(): TPTP.AnnotatedFormula = {
+      import modules.input.TPTPParser.annotatedTHF
+      annotatedTHF("thf(mrel_type, type, mrel: mworld > mworld > $$o).")
+    }
+
+    private[this] def indexedAccessibilityRelationTPTPDef(typ: String): TPTP.AnnotatedFormula = {
+      import modules.input.TPTPParser.annotatedTHF
+      annotatedTHF(s"thf(mrel_type, type, mrel: $typ > mworld > mworld > $$o).")
+    }
+
+    private[this] def mglobalTPTPDef(): Seq[TPTP.AnnotatedFormula] = {
+      import modules.input.TPTPParser.annotatedTHF
+      Seq(
+        annotatedTHF("thf(mglobal_type, type, mglobal: (mworld > $o) > $o)."),
+        annotatedTHF("thf(mglobal_def, type, mglobal = (^ [Phi: mworld > $o]: ![W: mworld]: (Phi @ W)) ).")
+      )
+    }
+
+    private[this] def mlocalTPTPDef(): Seq[TPTP.AnnotatedFormula] = {
+      import modules.input.TPTPParser.annotatedTHF
+      Seq(
+        annotatedTHF("thf(mactual_type, type, mactual: mworld)."),
+        annotatedTHF("thf(mlocal_type, type, mglobal: (mworld > $o) > $o)."),
+        annotatedTHF("thf(mlocal_def, type, mglobal = (^ [Phi: mworld > $o]: (Phi @ mactual)) ).")
+      )
+    }
+
+    private[this] def connectivesTPTPDef(): Seq[TPTP.AnnotatedFormula] = {
+      import modules.input.TPTPParser.annotatedTHF
+      Seq(
+        annotatedTHF("thf(mnot_type, type , ( mnot: (mworld>$o)>mworld>$o) )."),
+        annotatedTHF("thf(mand_type, type , ( mand: (mworld>$o)>(mworld>$o)>mworld>$o) )."),
+        annotatedTHF("thf(mor_type, type , ( mor: (mworld>$o)>(mworld>$o)>mworld>$o) )."),
+        annotatedTHF("thf(mimplies_type, type , ( mimplies: (mworld>$o)>(mworld>$o)>mworld>$o) )."),
+        annotatedTHF("thf(mequiv_type, type , ( mequiv: (mworld>$o)>(mworld>$o)>mworld>$o) )."),
+        annotatedTHF("thf(mnot_def, definition , ( mnot = (^ [A:mworld>$o,W:mworld] : ~(A@W))))."),
+        annotatedTHF("thf(mand_def, definition , ( mand = (^ [A:mworld>$o,B:mworld>$o,W:mworld] : ( (A@W) & (B@W) ))))."),
+        annotatedTHF("thf(mor_def, definition , ( mor = (^ [A:mworld>$o,B:mworld>$o,W:mworld] : ( (A@W) | (B@W) ))))."),
+        annotatedTHF("thf(mimplies_def, definition , ( mimplies = (^ [A:mworld>$o,B:mworld>$o,W:mworld] : ( (A@W) => (B@W) ))))."),
+        annotatedTHF("thf(mequiv_def, definition , ( mequiv = (^ [A:mworld>$o,B:mworld>$o,W:mworld] : ( (A@W) <=> (B@W) )))).")
+      )
+    }
+
+    private[this] def simpleModalOperatorsTPTPDef(): Seq[TPTP.AnnotatedFormula] = {
+      import modules.input.TPTPParser.annotatedTHF
+      Seq(
+        annotatedTHF("thf(mbox_type, type, mbox: (mworld>$o)>mworld>$o )."),
+        annotatedTHF("thf(mbox_def, definition, ( mbox = (^ [Phi:mworld>$o, W:mworld]: ![V:mworld]: ( (mrel @ W @ V) => (Phi @ V) )))).")
+      )
+    }
+
+    private[this] def indexedModalOperatorsTPTPDef(typ: String): Seq[TPTP.AnnotatedFormula] = {
+      import modules.input.TPTPParser.annotatedTHF
+      Seq(
+        annotatedTHF(s"thf(mbox_type, type, mbox: $typ > (mworld>$$o)>mworld>$$o )."),
+        annotatedTHF(s"thf(mbox_def, definition, ( mbox = (^ [R:$typ, A:mworld>$$o,W:mworld]: ! [V:mworld]: ( (mrel@R@W@V) => (A@V) )))).")
+      )
+    }
+
     private[this] def worldType: TPTP.THF.Type = {
       THF.FunctionTerm("mworld", Seq.empty)
     }
@@ -275,6 +374,11 @@ object ModalEmbedding {
     //////////////////////////////////////////////////////////////////////
     // Logic specification parsing
     //////////////////////////////////////////////////////////////////////
+    import collection.mutable
+    private[this] final val modalOperators: mutable.Buffer[String] = mutable.Buffer.empty
+    private[this] def isMultiModal: Boolean = modalOperators.nonEmpty
+    private[this] def multiModal(identifier: String): Unit = modalOperators += identifier
+
 
     private[this] def createState(spec: TPTP.AnnotatedFormula, options: Set[EmbeddingOption]): Unit = {
       if (options contains EmbeddingOption.POLYMORPHIC) state(POLYMORPHISM) += (() -> true)
